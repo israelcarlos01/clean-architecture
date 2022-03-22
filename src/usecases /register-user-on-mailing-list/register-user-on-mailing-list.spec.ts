e097762882e9928e3be1b1da0adb90bd32daf9c4
Israel Carlos <israel.carlos@vamosparcelar.gov.br>
2022-03-21 22:12:01 -0300

    feat: user data interface

---
+++ src/usecases /register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { IUserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: IUserData[] = []
+    console.log(users)
+    // const repo: userRepository = new InMemoryUserRepository(users)
+    // const useCase: registerUserOnMailingList = new registerUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await useCase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
