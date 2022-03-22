cb49fcc4343446f6f9a5e080717c468cad6a9649
Israel Carlos <israel.carlos@vamosparcelar.gov.br>
2022-03-22 17:13:59 -0300

    feat: add and find user and checks its existence

--- src/usecases /register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases /register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -8,11 +8,19 @@ export class InMemoryUserRepository implements IUserRepository {
       this.repository = repository
     }
 
-    add (user: IUserData): Promise<void> {
-      throw new Error('Method not implemented.')
+    async add (user: IUserData): Promise<void> {
+      const exists = await this.exists(user)
+      if (!exists) {
+        this.repository.push(user)
+      }
     }
 
-    findUserByEmail (email: string): Promise<IUserData> {
+    async findUserByEmail (email: string): Promise<IUserData> {
+      const users = this.repository.filter(user => user.email === email)
+
+      if (users.length > 0) {
+        return users[0]
+      }
       return null
     }
 
@@ -20,7 +28,11 @@ export class InMemoryUserRepository implements IUserRepository {
       throw new Error('Method not implemented.')
     }
 
-    exists (user: IUserData): Promise<boolean> {
-      throw new Error('Method not implemented.')
+    async exists (user: IUserData): Promise<boolean> {
+      const isExist = await this.findUserByEmail(user.email)
+      if (!isExist) {
+        return false
+      }
+      return true
     }
 }
