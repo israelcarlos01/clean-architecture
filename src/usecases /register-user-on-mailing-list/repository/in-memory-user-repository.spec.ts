07c7fc6f1c3ddb474316367029e4396ee4643c17
Israel Carlos <israel.carlos@vamosparcelar.gov.br>
2022-03-22 17:23:14 -0300

    test: return all users in the repository

--- src/usecases /register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases /register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -5,17 +5,27 @@ describe('In memory User repository', () => {
   test('shold return null if user is not found', async () => {
     const users: IUserData[] = []
     const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUserByEmail('any@gmail.com')
+    const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
   })
 
   test('shold return user if it is found in the repository', async () => {
     const users: IUserData[] = []
     const name = 'any_name'
-    const email = 'any@email.com'
+    const email = 'any@mail.com'
     const userRepo = new InMemoryUserRepository(users)
     await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail('any@email.com')
+    const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user.name).toBe('any_name')
+  })
+
+  test('shold return all users in the repository', async () => {
+    const users: IUserData[] = [
+      { name: 'any_name', email: 'any@mail.com' },
+      { name: 'second_name', email: 'second@mail.com' }
+    ]
+    const userRepo = new InMemoryUserRepository(users)
+    const returnedUsers = userRepo.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
   })
 })
